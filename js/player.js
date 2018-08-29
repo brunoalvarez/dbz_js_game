003851849eef2f7d89d1b42fd5860ccb1c351032
Bruno Alvarez <brunoalvarez2@gmail.com>
2018-08-28 23:30:11 -0300

    pequena implementação de tentativa de evento de pulo sem gravidade

--- js/player.js
+++ js/player.js
@@ -10,8 +10,8 @@ playerModel = {
     xInitialPosition: 0,
     xCurrentPosition: 0,
     yCurrentPosition: 0,
-    velocity:3 ,
-    jump : {
+    velocity: 3,
+    jump: {
         yLimit: -200
     },
 };
@@ -19,64 +19,59 @@ playerModel = {
 
 var player_action = player_action || {
 
-    
+
     collision: false,
 
     walk: function (left) {
         this.collisionScenario(left);
         if (!this.collision) {
-            playerModel.xCurrentPosition = left 
-            ? (playerModel.xCurrentPosition - playerModel.velocity)
-            : (playerModel.xCurrentPosition + playerModel.velocity)
-            ;
+            playerModel.xCurrentPosition = left
+                ? (playerModel.xCurrentPosition - playerModel.velocity)
+                : (playerModel.xCurrentPosition + playerModel.velocity)
+                ;
         }
 
         goku.dash();
-        
+
         $playerElement.css("margin-left", playerModel.xCurrentPosition);
-       
+
     },
 
-    jump: function(left)
-    {
+    jump: function (left) {
         //A cada 1 seg 
 
         this.collisionScenario(left);
         if (!this.collision) {
-            // playerModel.xCurrentPosition = left 
-            // ? (playerModel.xCurrentPosition - playerModel.velocity)
-            // : (playerModel.xCurrentPosition + playerModel.velocity)
-            // ;
+
             setInterval(function () {
-                //debugger;
-
-                if(parseInt($("#player1").css("margin-top")) > playerModel.jump.yLimit)
-                 {                   
-                    playerModel.yCurrentPosition =   parseInt($("#player1").css("margin-top")); 
-                    $("#player1").css("margin-top", playerModel.yCurrentPosition -1 + "px");
-
-                 }
-              }, 100);
+                if (parseInt($("#player1").css("margin-top")) > playerModel.jump.yLimit) {
+                    playerModel.yCurrentPosition = parseInt($("#player1").css("margin-top"));
+                    $("#player1").css("margin-top", playerModel.yCurrentPosition - 15 + "px");
+                }
+                
+                else {
+                    playerModel.yCurrentPosition = parseInt($("#player1").css("margin-top"));
+                    $("#player1").css("margin-top", playerModel.yCurrentPosition + 15 + "px");
+                }
+            }, 100);
         }
     },
 
     collisionScenario: function (left) {
-        
+
         var currentMarginLeft = parseInt($playerElement.css("margin-left"));
 
-        if(left && currentMarginLeft == playerModel.limitsX[0])
-        {
+        if (left && currentMarginLeft == playerModel.limitsX[0]) {
             this.collision = true;
-            return;    
-        }       
-        if(!left && currentMarginLeft >= playerModel.limitsX[1])
-        {
+            return;
+        }
+        if (!left && currentMarginLeft >= playerModel.limitsX[1]) {
             this.collision = true;
             return;
         }
 
         this.collision = false;
-       
+
     }
 };
 
